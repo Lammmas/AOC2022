bbf49ad51f2e6b3ba861fbfa874eae08b09a46ea
Renna <renna@reemet.me>
2022-12-11 13:45:49 +0000

    day 6

--- 6.py
+++ 6.py
@@ -1,11 +1,23 @@
 from in6 import *
 
-inarr = text.splitlines()
-
 # Part 1
-answer1 = 0
+idx = 0
+for i in range(4, len(text[3:])):
+    lastChars = text[i - 4:i]
+    if len(set(lastChars)) == len(lastChars):
+        idx = i
+        break
+
+answer1 = idx
 
 # Part 2
-answer2 = 0
+idx = 0
+for i in range(14, len(text[13:])):
+    lastChars = text[i - 14:i]
+    if len(set(lastChars)) == len(lastChars):
+        idx = i
+        break
+
+answer2 = idx
 
 print(f'First: {answer1} Second: {answer2}')
